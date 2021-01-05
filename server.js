48ee27ad9c012065f686ef8dd49cbc66713fc0ec
akadiving <akoivanishvili6@gmail.com>
2021-01-05 15:09:38 +0400

    new update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const history = require('connect-history-api-fallback');const app = express();
-app.use(history());
-app.use(express.static('src'));app.get('/', (req, res) => {
-  res.sendFile('src/index.html');
-});app.listen(3000, () => console.log('server started'));
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+const app = express()
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+const port = process.env.PORT || 8080
+app.listen(port)
