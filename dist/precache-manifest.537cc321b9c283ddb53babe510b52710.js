e86e100a8830264b81a76da05f5f0dbe02ce40f0
akadiving <akoivanishvili6@gmail.com>
2021-01-05 14:29:55 +0400

    built for heroku

---
+++ dist/precache-manifest.537cc321b9c283ddb53babe510b52710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ee8403b1020ce2f27f",
+    "url": "/css/app.b055fbd6.css"
+  },
+  {
+    "revision": "8f8ff0f2f1f336622171",
+    "url": "/css/chunk-vendors.cb0814db.css"
+  },
+  {
+    "revision": "5a513a0864d06cdf25b9fd9bedb1d1ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ee8403b1020ce2f27f",
+    "url": "/js/app.4e6e54d6.js"
+  },
+  {
+    "revision": "8f8ff0f2f1f336622171",
+    "url": "/js/chunk-vendors.c42a5e92.js"
+  },
+  {
+    "revision": "25729cd1a77ce533bbece4b46fc9f7c4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
