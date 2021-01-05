a6b2112361cc399ece804f7308251ee33c3e6026
akadiving <akoivanishvili6@gmail.com>
2021-01-05 14:57:50 +0400

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
-
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function(req, res) {
-    res.sendfiles(__dirname + "/dist/index.html");
-});
-app.listen(port)
-
-console.log("Server started...")
+const history = require('connect-history-api-fallback');const app = express();
+app.use(history());
+app.use(express.static('src'));app.get('/', (req, res) => {
+  res.sendFile('src/index.html');
+});app.listen(3000, () => console.log('server started'));
